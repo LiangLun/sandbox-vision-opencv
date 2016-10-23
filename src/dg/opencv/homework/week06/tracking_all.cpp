dc750532a565a1071b1541801191b74bc42e3ee0
xenron <xenron@hotmail.com>
2016-10-24 00:28:51 +0900

    msg

--- src/dg/opencv/homework/week06/tracking_all.cpp
+++ src/dg/opencv/homework/week06/tracking_all.cpp
@@ -25,31 +25,31 @@ int main(int argc, char* argv[])
     }
 
     // create a tracker object
-    cv::Ptr<cv::Tracker> trackerKCF = cv::Tracker::create("KCF");
-    cv::Ptr<cv::Tracker> trackerTLD = cv::Tracker::create("TLD");
-    cv::Ptr<cv::Tracker> trackerMEDIANFLOW = cv::Tracker::create("MEDIANFLOW");
-    cv::Ptr<cv::Tracker> trackerBOOSTING = cv::Tracker::create("BOOSTING");
-    cv::Ptr<cv::Tracker> trackerMIL = cv::Tracker::create("MIL");
+    Ptr<Tracker> trackerKCF = Tracker::create("KCF");
+    Ptr<Tracker> trackerTLD = Tracker::create("TLD");
+    Ptr<Tracker> trackerMEDIANFLOW = Tracker::create("MEDIANFLOW");
+    Ptr<Tracker> trackerBOOSTING = Tracker::create("BOOSTING");
+    Ptr<Tracker> trackerMIL = Tracker::create("MIL");
 
-    cv::VideoCapture cap(argv[1]);
+    VideoCapture cap(argv[1]);
     if (!cap.isOpened()) {
-        std::cout << "error" << std::endl;
+        cout << "error" << endl;
         return -1;
     }
 
 
-    cv::Mat frame;
+    Mat frame;
     cap >> frame;
 
-    cv::Rect2d roi = cv::selectROI("tracker", frame);
-    cv::Rect2d roiTLD = roi;
-    cv::Rect2d roiMEDIANFLOW = roi;
-    cv::Rect2d roiBOOSTING = roi;
-    cv::Rect2d roiMIL = roi;
+    Rect2d roi = selectROI("tracker", frame);
+    Rect2d roiTLD = roi;
+    Rect2d roiMEDIANFLOW = roi;
+    Rect2d roiBOOSTING = roi;
+    Rect2d roiMIL = roi;
 
-    cv::Mat target(frame, roi);
-    cv::imwrite("target.jpeg", target);
-    std::cout << "(x, y, width, height) = (" << roi.x << "," << roi.y << "," << roi.width << "," << roi.height << ")" << std::endl;
+    Mat target(frame, roi);
+    imwrite("target.jpeg", target);
+    cout << "(x, y, width, height) = (" << roi.x << "," << roi.y << "," << roi.width << "," << roi.height << ")" << endl;
 
     if (roi.width == 0 || roi.height == 0)
         return -1;
@@ -62,18 +62,18 @@ int main(int argc, char* argv[])
     trackerMIL->init(frame, roiMIL);
 
     // Tracker color setting
-    cv::Scalar colorkcf = cv::Scalar(0, 255, 0);
-    cv::Scalar colortld = cv::Scalar(0, 255, 255);
-    cv::Scalar colormedianflow = cv::Scalar(0, 0, 255);
-    cv::Scalar colorboosting = cv::Scalar(255, 255, 0);
-    cv::Scalar colormit = cv::Scalar(255, 0, 255);
+    Scalar colorkcf = Scalar(0, 255, 0);
+    Scalar colortld = Scalar(0, 255, 255);
+    Scalar colormedianflow = Scalar(0, 0, 255);
+    Scalar colorboosting = Scalar(255, 255, 0);
+    Scalar colormit = Scalar(255, 0, 255);
 
     // writer video
     double fps = cap.get(CV_CAP_PROP_FPS);
-    cv::Size size = cv::Size(cap.get(CV_CAP_PROP_FRAME_WIDTH), cap.get(CV_CAP_PROP_FRAME_HEIGHT));
-    const int fourcc = cv::VideoWriter::fourcc('X', 'V', 'I', 'D');
+    Size size = Size(cap.get(CV_CAP_PROP_FRAME_WIDTH), cap.get(CV_CAP_PROP_FRAME_HEIGHT));
+    const int fourcc = VideoWriter::fourcc('X', 'V', 'I', 'D');
     std::string filename = "output.avi";
-    cv::VideoWriter writer(filename, fourcc, fps, size);
+    VideoWriter writer(filename, fourcc, fps, size);
 
     while (1) {
         cap >> frame;
@@ -88,22 +88,22 @@ int main(int argc, char* argv[])
         trackerMIL->update(frame, roiMIL);
 
         // draw rectangle
-        cv::rectangle(frame, roi, colorkcf, 1, 1);
-        cv::rectangle(frame, roiTLD, colortld, 1, 1);
-        cv::rectangle(frame, roiMEDIANFLOW, colormedianflow, 1, 1);
-        cv::rectangle(frame, roiBOOSTING, colorboosting, 1, 1);
-        cv::rectangle(frame, roiMIL, colormit, 1, 1);
+        rectangle(frame, roi, colorkcf, 1, 1);
+        rectangle(frame, roiTLD, colortld, 1, 1);
+        rectangle(frame, roiMEDIANFLOW, colormedianflow, 1, 1);
+        rectangle(frame, roiBOOSTING, colorboosting, 1, 1);
+        rectangle(frame, roiMIL, colormit, 1, 1);
 
-        cv::putText(frame, "- KCF", cv::Point(5, 20), cv::FONT_HERSHEY_SIMPLEX, .5, colorkcf, 1, CV_AA);
-        cv::putText(frame, "- TLD", cv::Point(65, 20), cv::FONT_HERSHEY_SIMPLEX, .5, colortld, 1, CV_AA);
-        cv::putText(frame, "- MEDIANFLOW", cv::Point(125, 20), cv::FONT_HERSHEY_SIMPLEX, .5, colormedianflow, 1, CV_AA);
-        cv::putText(frame, "- BOOSTING", cv::Point(5, 40), cv::FONT_HERSHEY_SIMPLEX, .5, colorboosting, 1, CV_AA);
-        cv::putText(frame, "- MIL", cv::Point(115, 40), cv::FONT_HERSHEY_SIMPLEX, .5, colormit, 1, CV_AA);
+        putText(frame, "- KCF", Point(5, 20), FONT_HERSHEY_SIMPLEX, .5, colorkcf, 1, CV_AA);
+        putText(frame, "- TLD", Point(65, 20), FONT_HERSHEY_SIMPLEX, .5, colortld, 1, CV_AA);
+        putText(frame, "- MEDIANFLOW", Point(125, 20), FONT_HERSHEY_SIMPLEX, .5, colormedianflow, 1, CV_AA);
+        putText(frame, "- BOOSTING", Point(5, 40), FONT_HERSHEY_SIMPLEX, .5, colorboosting, 1, CV_AA);
+        putText(frame, "- MIL", Point(115, 40), FONT_HERSHEY_SIMPLEX, .5, colormit, 1, CV_AA);
 
-        cv::imshow("tracker", frame);
+        imshow("tracker", frame);
         writer << frame;
 
-        int key = cv::waitKey(30);
+        int key = waitKey(30);
         if (key == 0x1b)
             break;
     }
